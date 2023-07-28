999a46ba5c33a2e877ecc7b9eedf5124af4bdf16
진충희 <choonghee.jin@samsung.com>
2023-07-28 17:31:02 +0900

    CC

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -5,52 +5,52 @@ import (
 )
 
 type Listener interface {
-    Listen(event interface{})
+	Listen(event any)
 }
 
 type job struct {
-    to string
-    do interface{}
+	to string
+	do any
 }
 
 type Dispatcher struct {
-    jobCh chan job
-    table map[string]Listener
+	jobCh chan job
+	table map[string]Listener
 }
 
 func NewDispatcher() *Dispatcher {
-    d := &Dispatcher{
-        jobCh: make(chan job, 10000),
-        table: make(map[string]Listener),
-    }
+	d := &Dispatcher{
+		jobCh: make(chan job, 10000),
+		table: make(map[string]Listener),
+	}
 
-    go func() {
-        for job := range d.jobCh {
-            d.table[job.to].Listen(job.do)
-        }
-    }()
+	go func() {
+		for job := range d.jobCh {
+			d.table[job.to].Listen(job.do)
+		}
+	}()
 
-    return d
+	return d
 }
 
-func (d *Dispatcher) Register(listener Listener, too ...string) error {
-    for _, to := range too {
-        if _, ok := d.table[to]; ok {
-            return fmt.Errorf("Already %s address is used", to)
-        }
+func (d *Dispatcher) Register(listener Listener, addresses ...string) error {
+	for _, addr := range addresses {
+		if _, ok := d.table[addr]; ok {
+			return fmt.Errorf("Already %s address is used", addr)
+		}
 
-        d.table[to] = listener
-    }
+		d.table[addr] = listener
+	}
 
-    return nil
+	return nil
 }
 
-func (d *Dispatcher) Dispatch(to string, do interface{}) error {
-    if _, ok := d.table[to]; !ok {
-        return fmt.Errorf("'%s' address is not registered", to)
-    }
+func (d *Dispatcher) Dispatch(to string, do any) error {
+	if _, ok := d.table[to]; !ok {
+		return fmt.Errorf("'%s' address is not registered", to)
+	}
 
-    d.jobCh <- job{to: to, do: do}
+	d.jobCh <- job{to: to, do: do}
 
-    return nil
+	return nil
 }
